ca2318974be7c4d9f26b6566c81b02fbe2bec8de
Henry Edwards <ym24345@bristol.ac.uk>
2025-11-13 12:07:54 +0000

    Update server.go

--- Distributedgol/server/server.go
+++ Distributedgol/server/server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"Distributedgol/stubs"
-	"Distributedgol/util"
 	"flag"
+	"fmt"
 	"net"
 	"net/rpc"
+	"os"
 	"sync"
+	"time"
+
+	"uk.ac.bris.cs/gameoflife/stubs"
+	"uk.ac.bris.cs/gameoflife/util"
 )
 
 type GameState struct {
@@ -15,6 +19,8 @@ type GameState struct {
 	Height int
 	Width  int
 	Turn   int
+	Paused bool
+	Quit   chan bool
 }
 
 func updateState(height int, width int, currentWorld [][]uint8, out chan [][]uint8, g *GameState) {
@@ -58,9 +64,6 @@ func updateState(height int, width int, currentWorld [][]uint8, out chan [][]uin
 			}
 		}
 	}
-	g.Lock.Lock()
-	g.World = nextWorld
-	g.Lock.Unlock()
 	out <- nextWorld
 }
 
@@ -76,15 +79,6 @@ func calculateAliveCells(imgHeight int, imgWidth int, world [][]byte) []util.Cel
 	return aliveCells
 }
 
-func (g *GameState) HandleAlive(req stubs.CellRequest, res *stubs.Response) error {
-	g.Lock.Lock()
-	defer g.Lock.Unlock()
-	aliveCells := len(calculateAliveCells(g.Height, g.Width, g.World))
-	res.Cells = aliveCells
-	res.Turn = g.Turn
-	return nil
-}
-
 func makeWorld(imgHeight int, imgWidth int, world [][]byte) [][]uint8 {
 	currentWorld := make([][]uint8, imgHeight)
 	for i := range currentWorld {
@@ -97,28 +91,90 @@ func makeWorld(imgHeight int, imgWidth int, world [][]byte) [][]uint8 {
 	}
 	return currentWorld
 }
+func switchPause(g *GameState, res *stubs.Response) error {
+	g.Lock.Lock()
+	defer g.Lock.Unlock()
+	g.Paused = !g.Paused
+	res.Paused = g.Paused
+	res.Turn = g.Turn
+	return nil
+}
+
+func saveWorld(g *GameState, res *stubs.Response) error {
+	g.Lock.Lock()
+	defer g.Lock.Unlock()
+	res.Message = g.World
+	res.Turn = g.Turn
+	return nil
+}
+func (g *GameState) HandleAlive(req stubs.CellRequest, res *stubs.Response) error {
+	g.Lock.Lock()
+	defer g.Lock.Unlock()
+	aliveCells := len(calculateAliveCells(g.Height, g.Width, g.World))
+	res.Cells = aliveCells
+	res.Turn = g.Turn
+	return nil
+}
+
+func (g *GameState) HandleKeyPress(req stubs.KeyRequest, res *stubs.Response) error {
+	keyPress := req.Key
+	switch {
+	case keyPress == 'p': // pause gol
+		return switchPause(g, res)
+	case keyPress == 's': // send pgm image of current gol
+		return saveWorld(g, res)
+	case keyPress == 'q': // send pgm image of final turn , then quit gol
+		saveWorld(g, res)
+	case keyPress == 'k':
+		saveWorld(g, res)
+	default:
+	}
+	return nil
+}
+
+func (g *GameState) HandleQuit(req stubs.QuitRequest, response *stubs.Response) error {
+	if req.Flag {
+		saveWorld(g, response)
+
+		go func() {
+			time.Sleep(1 * time.Second)
+			g.Quit <- true
+		}()
+
+	}
+	return nil
+}
 
 func (g *GameState) HandleState(req stubs.Request, res *stubs.Response) error {
+	currentWorld := makeWorld(req.ImgHeight, req.ImgWidth, req.Message)
 	g.Lock.Lock()
 	g.Height = req.ImgHeight
 	g.Width = req.ImgWidth
+	g.World = currentWorld
+	g.Paused = false
 	g.Lock.Unlock()
-	currentWorld := makeWorld(req.ImgHeight, req.ImgWidth, req.Message)
 	channel := make(chan [][]uint8)
 	turn := 0
-	for i := 0; i < req.Turns; i++ {
-		nextWorld := makeWorld(req.ImgHeight, req.ImgWidth, currentWorld)
-		go updateState(req.ImgHeight, req.ImgWidth, currentWorld, channel, g)
-		nextWorld = <-channel
-		currentWorld, nextWorld = nextWorld, currentWorld
-		turn += 1
-		g.Lock.Lock()
-		g.Turn = turn
-		g.Lock.Unlock()
+	for i := 0; i < req.Turns; {
+		if !g.Paused {
+			nextWorld := makeWorld(req.ImgHeight, req.ImgWidth, currentWorld)
+			go updateState(req.ImgHeight, req.ImgWidth, currentWorld, channel, g)
+			nextWorld = <-channel
+			currentWorld, nextWorld = nextWorld, currentWorld
+			turn += 1
+			g.Lock.Lock()
+			g.Turn = turn
+			fmt.Println(turn)
+			g.World = currentWorld
+			g.Lock.Unlock()
+			i++
+		} else { // wait until world is unpaused to continue game
+			for g.Paused {
+				time.Sleep(50 * time.Millisecond)
+			}
+		}
+
 	}
-	g.Lock.Lock()
-	g.World = currentWorld
-	g.Lock.Unlock()
 	res.Message = g.World
 	res.Turn = turn
 	return nil
@@ -127,11 +183,18 @@ func (g *GameState) HandleState(req stubs.Request, res *stubs.Response) error {
 func main() {
 	pAddr := flag.String("port", "8020", "Port to listen on")
 	flag.Parse()
-	rpc.Register(&GameState{})
+
+	g := &GameState{Quit: make(chan bool)}
+	rpc.Register(g)
+
 	listener, err := net.Listen("tcp", ":"+*pAddr)
 	if err != nil {
 		panic(err)
 	}
 	defer listener.Close()
-	rpc.Accept(listener)
+
+	go rpc.Accept(listener)
+	<-g.Quit
+	fmt.Println("Shutting down...")
+	os.Exit(0)
 }
