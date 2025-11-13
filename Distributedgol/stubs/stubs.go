9c5904f07a13e78502f65f893a19452cc7fef2c7
Henry Edwards <ym24345@bristol.ac.uk>
2025-11-13 12:12:22 +0000

    Update stubs.go

--- Distributedgol/stubs/stubs.go
+++ Distributedgol/stubs/stubs.go
@@ -1,9 +1,15 @@
 package stubs
 
+var StateHandler = "GameState.HandleState"
+var CellHandler = "GameState.HandleAlive"
+var KeyHandler = "GameState.HandleKeyPress"
+var QuitHandler = "GameState.HandleQuit"
+
 type Response struct {
 	Message [][]uint8
 	Cells   int
 	Turn    int
+	Paused  bool
 }
 
 type Request struct {
@@ -14,5 +20,13 @@ type Request struct {
 }
 
 type CellRequest struct {
+	Flag bool
+}
+
+type KeyRequest struct {
+	Key rune
+}
+
+type QuitRequest struct {
 	Flag bool
 }
